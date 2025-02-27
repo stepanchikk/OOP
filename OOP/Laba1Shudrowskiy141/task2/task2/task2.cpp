c99b83df645fb743f5a1c6e00f83a4f6e89193f6
stepanchikk <122825623+stepanchikk@users.noreply.github.com>
2025-02-27 22:15:15 +0200

    Add files via upload

---
+++ OOP/Laba1Shudrowskiy141/task2/task2/task2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <climits>
+
+int main() {
+    int N, k1;
+    std::cout << "Enter the array size: ";
+    std::cin >> N;
+
+    std::vector<int> A(N);
+    std::cout << "Enter the array elements: ";
+    for (int i = 0; i < N; ++i) {
+        std::cin >> A[i];
+    }
+
+    std::cout << "Enter k1: ";
+    std::cin >> k1;
+
+    int* pA = A.data();
+    int max_value = INT_MIN;
+
+    for (int i = 0; i < N; ++i, ++pA) {
+        if (*pA < 0) {
+            break;
+        }
+        if (*pA % k1 == 0 && *pA > max_value) {
+            max_value = *pA;
+        }
+    }
+
+    if (max_value == INT_MIN) {
+        std::cout << "No valid element found." << std::endl;
+    }
+    else {
+        std::cout << "Maximum element divisible by " << k1 << " before first negative element: " << max_value << std::endl;
+    }
+
+    return 0;
+}
